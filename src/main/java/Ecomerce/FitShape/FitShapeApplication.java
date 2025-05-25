9c4eb4aa8470128a3c514e86ee2e6044f0ac4c5c
Nikson Calazans <131991262+NiksonMoraes@users.noreply.github.com>
2025-05-25 11:03:19 -0300

    salvando o projeto no repositorio do git

---
+++ src/main/java/Ecomerce/FitShape/FitShapeApplication.java
@@ -0,0 +1,13 @@
+package Ecomerce.FitShape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitShapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitShapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Ecomerce/FitShape/FitShapeApplicationTests.java
@@ -0,0 +1,13 @@
+package Ecomerce.FitShape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitShapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
